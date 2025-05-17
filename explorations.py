cce7d6c5bb63e31e3a2a0ff1e38db85a504667bf
jdranczewski <domdlapsa@gmail.com>
2025-05-17 10:38:42 +0100

    Basics

---
+++ explorations.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Explorations
+# Various tests of the API and the maths involved.
+
+# %%
